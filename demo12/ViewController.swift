ace1f16e385d86d4128dc8908913c7e329c2ef1f
mac <mac@macs-MacBook-Air.local>
2019-05-22 16:12:17 +0530

    Project setup

---
+++ demo12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demo12
+//
+//  Created by mac on 5/22/19.
+//  Copyright © 2019 BLH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
